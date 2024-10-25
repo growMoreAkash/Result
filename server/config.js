d945ccbab583f7e49046f17e8fac133abbe545ea
growMoreAkash <akashgrowmore@gmail.com>
2024-10-26 00:47:08 +0530

    done

--- server/config.js
+++ server/config.js
@@ -3,7 +3,5 @@ export default {
     MONGO_USER:'lizaroyjmd',
     MONGO_PASS:'VWJKXoBdFgpM0QQc',
     PORT:3001,
-    EMAIL:'debakash00000@gmail.com',
-    EMAIL_PASS:'egpludytbpzbpckz',
     host:'http://localhost:3001/api'
 }
